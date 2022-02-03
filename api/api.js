06f79c323c5944d7ebb3efe0e3a255860ae06315
Ricardo Barbosa <ricardo.barbosa@wexinc.com>
2022-02-03 10:29:30 -0300

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,6 +1,12 @@
 'use strict'
 
 exports.handler = function (event, context, callback) {
-    const response = event;
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            input: event,
+        }),
+    };
   callback(null, response)
 }
