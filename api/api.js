86cfda093ee55886f9c4522371027fef538a6a87
Ricardo Barbosa <ricardo.barbosa@wexinc.com>
2022-02-02 22:41:17 -0300

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,13 +1,6 @@
 'use strict'
 
 exports.handler = function (event, context, callback) {
-  var response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    body: 'Welcome to our demo API, here are the details of your request:\nHeaders:\nMethod:\nBody:',
-    method: method
-  }
+  var response = event
   callback(null, response)
 }
