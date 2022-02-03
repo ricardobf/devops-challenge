7ab04e7e3a9b2f37b44743348ea231530463ecf0
Ricardo Barbosa <ricardo.barbosa@wexinc.com>
2022-02-02 22:14:03 -0300

    Update response

--- api/api.js
+++ api/api.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
     },
-    body: '<h2>Welcome to our demo API, here are the details of your request: a</h2>',
+    body: 'Welcome to our demo API, here are the details of your request:\nHeaders:\nMethod:',
   }
   callback(null, response)
 }
