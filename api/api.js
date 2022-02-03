e832a812f54a7f8af3964c7f3ceed0db62920c06
Ricardo Barbosa <ricardo.barbosa@wexinc.com>
2022-02-02 22:50:52 -0300

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,10 +1,6 @@
 'use strict'
 
 exports.handler = function (event, context, callback) {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Welcome to our demo API, here are the details of your request:',
-         event),
-    };
+    const response = event;
   callback(null, response)
 }
