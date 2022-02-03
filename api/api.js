dee918dd16cc321c57c561706ef6bdf52973fbc8
Ricardo Barbosa <ricardo.barbosa@wexinc.com>
2022-02-03 10:31:28 -0300

    Update api.js

--- api/api.js
+++ api/api.js
@@ -4,8 +4,10 @@ exports.handler = function (event, context, callback) {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event,
+            message: 'Welcome to our demo API, here are the details of your request:',
+            Headers: event.Headers,
+            Method: event.Method,
+            Body: event.body
         }),
     };
   callback(null, response)
